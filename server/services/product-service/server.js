45edb6f8064a3033ddba1845a248c46697c79672
Tấn Dự <tandu542003@gmail.com>
2025-03-14 10:50:23 +0700

    be/create product-service

---
+++ server/services/product-service/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./src/config/database");
+const productRoutes = require("./src/routes/product.routes");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5002;
+
+// Enable All CORS Requests
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Kết nối MongoDB
+connectDB();
+
+// Routes
+app.use("/api/products", productRoutes);
+
+// Run server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
